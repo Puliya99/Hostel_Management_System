ecbf5b3da0deb267d564ac028cd13ac547633d39
lmarcho <lakshitha.mathngadeera@neosolax.com>
2023-09-06 00:37:50 +0530

    Connect hibernate for Hostel_Management_System

---
+++ src/main/java/lk/ijse/hostelManagementSystem/util/FactoryConfiguration.java
@@ -0,0 +1,26 @@
+package lk.ijse.hostelManagementSystem.util;
+
+import lk.ijse.hostelManagementSystem.entity.Reservation;
+import lk.ijse.hostelManagementSystem.entity.Room;
+import lk.ijse.hostelManagementSystem.entity.Student;
+import lk.ijse.hostelManagementSystem.entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+    private FactoryConfiguration(){
+        Configuration configuration =new Configuration().configure().addAnnotatedClass(Student.class).addAnnotatedClass(Room.class).addAnnotatedClass(Reservation.class).addAnnotatedClass(User.class);
+        sessionFactory = configuration.buildSessionFactory();
+    }
+    public static FactoryConfiguration getInstance(){
+        return (factoryConfiguration==null)?
+                factoryConfiguration=new FactoryConfiguration():factoryConfiguration;
+    }
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+
+}
